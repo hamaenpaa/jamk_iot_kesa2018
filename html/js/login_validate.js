ee91c7017b85526ff00c983b0648f7707ce22303
Tomi Ehrukainen <Mursa@hotmail.fi>
2018-07-13 01:48:34 +0300

    OCD hitting hard if comments are not good.

--- html/js/login_validate.js
+++ html/js/login_validate.js
@@ -1,17 +1,18 @@
-//Requires jQuery library to work, change utag and ptag to referral if required.
+//Requires jQuery library to work, change utag, ptag and loginsubmit to related form fields.
 $(function() {
 	loginsubmit = $("#loginsubmit"); //Login submit element (input type=submit)
 	loginsubmit.prop("disabled", true); //Disables the submit button untill proper validation is done
 	//On username, password input field keypress -> run function
-		$("#username, #password").on("keyup", function() {
-		utag = $("#username"); //Creates user id input to variable
-		ptag = $("#password"); //Creates password id input to variable
-		passlen = ptag.val().length; //Gets password length
-		regexp = /[a-zA-Z0-9]+/; //Regex pattern allow any characters from (a-z A-Z and 0-9).
-		matches = utag.val().match(regexp); //Validates username with regexp
-		lp1 = false; //Login pass -> (username)
-		
-		if (matches != null) {
+	$("#username, #password").on("keyup", function() {
+	utag = $("#username"); //Gets element data from input where id = username
+	ptag = $("#password"); //Gets element data from input where id = password
+	passlen = ptag.val().length; //Gets password length
+	regexp = /[a-zA-Z0-9]+/; //Regex pattern allow any characters from (a-z A-Z and 0-9).
+	matches = utag.val().match(regexp); //Validates username with regexp
+	lp1 = false; //Login pass -> (username)
+	lp2 = false; //Login pass -> (password)
+	
+		if (matches != null) { //Checks if matches is null (incase user starts from password field instead of username)
 		userlen = matches[0].length; //Gets updated username length
 		$("#username").val(matches); //Removes any excess characters
 			if (userlen >= 5 && userlen <= 320) { //Username between 5 and 320 characters -> depending if email is login tool
@@ -23,7 +24,6 @@ $(function() {
 			}
 		}
 			
-		lp2 = false; //Login pass -> (password)
 		//Same as above, but for password
 		if (passlen >= 5 && passlen <= 150) {
 		ptag.css({"color":"green"});

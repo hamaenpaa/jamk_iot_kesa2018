6e724cf7098a85153c64737f7915ca42622c5153
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-07-16 10:05:12 +0300

    Broken version of data structure and test sql for fixing

--- docs/data_structure.sql
+++ docs/data_structure.sql
@@ -1,82 +1,82 @@
 DROP DATABASE IF EXISTS ca;
-CREATE DATABASE ca;
+CREATE DATABASE ca DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS ca_staff;
 CREATE TABLE ca_staff (
-    ID INT(11) NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT,
 	FirstName varchar(25) NOT NULL,
     LastName varchar(25) NOT NULL,
-    Email varchar(320) NOT NULL UNIQUE,
-    PhoneNumber varchar(13),
+    Email varchar(255) NOT NULL UNIQUE,
 	Permission TINYINT(1) DEFAULT 0, # admin=1 / teacher=0
 	Password varchar(64), #sha 256
 	Token varchar(32), # md5
-	Active TINYINT(1),
+	Active TINYINT(1),    
+	PhoneNumber varchar(13),
     PRIMARY KEY (ID)
-); 
+) DEFAULT CHARSET=utf8; 
 
 DROP TABLE IF EXISTS ca_staff_failed_login_log;
 CREATE TABLE ca_staff_failed_login_log (
-    ID INT(11) NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT,
 	user_ip VARCHAR(15) NOT NULL,
 	dt DATETIME NOT NULL,
 	PRIMARY KEY (ID)
-);
+) DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS ca_student;
 CREATE TABLE ca_student (
-    ID INT(11) NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT,
 	Student_ID CHAR(6) NOT NULL, # L99.. etc.
 	FirstName varchar(25) NOT NULL,
     LastName varchar(25) NOT NULL,
-    Email varchar(320) NOT NULL UNIQUE,
+    Email varchar(255) NOT NULL UNIQUE,
     PhoneNumber varchar(13) NOT NULL,
 	NFC_ID varchar(50) NOT NULL UNIQUE, 
     PRIMARY KEY (ID)
-); 
+) DEFAULT CHARSET=utf8; 
 
 DROP TABLE IF EXISTS ca_guest;
 CREATE TABLE ca_guest (
-    ID INT(11) NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT,
 	FirstName varchar(25) NOT NULL,
     LastName varchar(25) NOT NULL,	
     PRIMARY KEY (ID)
-); 
+) DEFAULT CHARSET=utf8; 
 
 DROP TABLE IF EXISTS ca_course;
 CREATE TABLE ca_course (
-    ID INT(11) NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT,
 	Course_ID VARCHAR(20), 
 	Course_name varchar(50) NOT NULL,
     Course_description varchar(500), 
     PRIMARY KEY (ID)
-); 
+) DEFAULT CHARSET=utf8; 
 
 DROP TABLE IF EXISTS ca_course_teacher;
 CREATE TABLE ca_course_teacher (
-    ID INT(11) NOT NULL AUTO_INCREMENT,
-	staff_id INT(11) NOT NULL,
-	course_id INT(11) NOT NULL,
+    ID INT NOT NULL AUTO_INCREMENT,
+	staff_id INT NOT NULL,
+	course_id INT NOT NULL,
     PRIMARY KEY (ID)
-);	
+) DEFAULT CHARSET=utf8;	
 
 DROP TABLE IF EXISTS ca_course_student;
 CREATE TABLE ca_course_student (
-    ID INT(11) NOT NULL AUTO_INCREMENT,
-	student_id INT(11) NOT NULL,
-	course_id INT(11) NOT NULL,
+    ID INT NOT NULL AUTO_INCREMENT,
+	student_id INT NOT NULL,
+	course_id INT NOT NULL,
     PRIMARY KEY (ID)
-);	
+) DEFAULT CHARSET=utf8;	
 	
 DROP TABLE IF EXISTS ca_roomlog;
 CREATE TABLE ca_roomlog (
-    ID INT(11) NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT,
 	NFC_ID varchar(50),
-	guest_id INT(11),
+	guest_id INT,
 	dt DATETIME NOT NULL,
-    room_id INT(11) NOT NULL,
+    room_id INT NOT NULL,
 	PRIMARY KEY (ID)
-);
+) DEFAULT CHARSET=utf8;
 
 INSERT INTO ca_staff (FirstName, LastName, Email, 
 Password,
--- test/testdata/testdata.sql
+++ test/testdata/testdata.sql
@@ -1,11 +1,11 @@
-# staff ( teachers )
+﻿-- staff ( teachers )
 INSERT INTO ca_staff 
 (FirstName,LastName,PhoneNumber,Email,
 Password,
 Active, Permission) 
 VALUES
 ('Niko', 'Kiviaho','0503888777', 'nikox@jamk.fi',
-'959f7afe3efbee4ebf60ac73120498b317126178a86181af0abda5612390b68b',
+'959f7afe3efbee4ebf60ac731204Q98b317126178a86181af0abda5612390b68b',
 1,0);
 INSERT INTO ca_staff 
 (FirstName,LastName,PhoneNumber,Email,
@@ -16,7 +16,7 @@ VALUES
 '2ed518af60c5c3b04fdbed95a7e6cd50aee2df23a8726e48303a2b8744dae2f8',
 1,0);
 
-# student
+-- student
 INSERT INTO ca_student
 (FirstName, LastName, PhoneNumber, Email, 
 Student_ID, NFC_ID)
@@ -53,14 +53,14 @@ VALUES
 'L9977','HEREETTT');
 
 
-# guest
+-- guest
 INSERT INTO ca_guest
 (FirstName,LastName)
 VALUES 
 ('Satu', 'Vesalainen');
 
 
-# courses
+-- courses
 INSERT INTO ca_course 
 (Course_ID,Course_name,Course_description) 
 VALUES 
@@ -73,13 +73,13 @@ VALUES
 ('RU-245', 'Ruotsin kielen alkeet','Perus small talk');
 
 
-# course teacher
+-- course teacher
 INSERT INTO ca_course_teacher (course_id, staff_id)
 VALUES (1,2);
 INSERT INTO ca_course_teacher (course_id, staff_id)
 VALUES (2,3);
 
-# course student
+-- course student
 INSERT INTO ca_course_student (course_id, student_id)
 VALUES (1,1);
 INSERT INTO ca_course_student (course_id, student_id)
@@ -91,7 +91,7 @@ VALUES (2,4);
 INSERT INTO ca_course_student (course_id, student_id)
 VALUES (2,5);
 
-# roomlog
+-- roomlog
 DELETE FROM ca_roomlog;
 
 

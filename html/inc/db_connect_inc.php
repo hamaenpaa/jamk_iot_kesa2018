c72d2ff5283f6f3a63cf537dbd56562cbbe88f6c
Tomi Ehrukainen <mursa@hotmail.fi>
2018-07-16 13:59:01 +0300

    Charset issue fixed

--- html/inc/db_connect_inc.php
+++ html/inc/db_connect_inc.php
@@ -1,9 +1,14 @@
 <?php
-   define("DB_HOST", "localhost");
-   define("DB_USER", "root");
-   define("DB_PASS", "");
-   define("DB_DATABASE", "ca");
+	define("DB_HOST", "localhost");
+	define("DB_USER", "root");
+	define("DB_PASS", "");
+	define("DB_DATABASE", "ca");
    
-   $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
-   mysqli_select_db($conn, DB_DATABASE);
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
+	mysqli_select_db($conn, DB_DATABASE);
+   
+	$conn->query('set character_set_client=utf8');
+	$conn->query('set character_set_connection=utf8');
+	$conn->query('set character_set_results=utf8');
+	$conn->query('set character_set_server=utf8');
 ?>

ba787b44a456870cb3f42247ef457835544c5fa1
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-07-18 15:50:17 +0300

    Student seek and refactorization implemented

--- html/inc/list_teachers.php
+++ html/inc/list_teachers.php
@@ -1,90 +1,16 @@
-
-
-
 <?php
-   include("db_connect_inc.php");
+    include("utils/request_param_utils.php");
+	include("utils/sql_utils.php");
 
-   if ($result = $conn->query("SELECT * FROM ca_staff WHERE Permission = 0 AND Active = 1")) {
-?>
-	<div id="active_staff_table">
-		<div class="row">
-			<div class="col-sm-3"><b>Etunimi</b></div>
-			<div class="col-sm-3"><b>Sukunimi</b></div>
-			<div class="col-sm-3"><b>Sähköposti</b></div>
-			<div class="col-sm-2"><b>Puhelin</b></div>
-			<div class="col-sm-1"><b>Muokkaa</b></div>
-		</div>
+    $seek_first_name = get_post_or_get("seek_first_name");
+	$seek_last_name = get_post_or_get("seek_last_name");
 
-<?php   	
-		while($res = $result->fetch_assoc()) {
-?>
-			<div class="row">
-				<div class="col-sm-3"><?php echo $res['FirstName']; ?></div>
-				<div class="col-sm-3"><?php echo $res['LastName']; ?></div>
-				<div class="col-sm-3"><?php echo $res['Email']; ?></div>
-				<div class="col-sm-2"><?php echo $res['PhoneNumber']; ?></div>
-				<div class="col-sm-1">
-					<form method="post" action="list_teachers.php">
-					<input type="hidden" name="id" value="<?php echo $res['ID']; ?>"/>
-					<input type="submit" value="Muokkaa" />
-					</form>
-				</div>				
-				
-			</div>
-<?php		
-		}
-?>
-	</div>
+	include("sub/teacher/seek_teachers_form.php");
 
-<?php
-	$teacher_firstname = "";
-	$teacher_lastname = "";
-	$teacher_email = "";
-	$teacher_phone = "";
-	$teacher_password = "";
-	$id = "";
-	if (isset($_POST['id'])) {
-		$id = $_POST['id'];
-		$q = $conn->prepare("SELECT FirstName,LastName,Email,PhoneNumber FROM ca_staff WHERE ID=?");
-		if ($q) {
-			$q->bind_param("s", $_POST['id']);
-			$q->execute();
-			$q->bind_result($teacher_firstname,$teacher_lastname,$teacher_email,$teacher_phone);
-			$q->fetch();
-			$q->close();
-		}
-	}
-		 
-?>	
-
+   	include("db_connect_inc.php");
 	
-	<form method="post" action="inc/save_teacher.php">
-		<input type="hidden" name="id" value="<?php echo $id; ?>" />
-		<label>Etunimi:</label>
-		<input type="text" name="teacher_firstname" value="<?php echo $teacher_firstname; ?>" />
-		<label>Sukunimi:</label>
-		<input type="text" name="teacher_lastname" value="<?php echo $teacher_lastname; ?>" />
-		<label>Sähköposti:</label>
-		<input type="text" name="teacher_email" value="<?php echo $teacher_email; ?>" />
-		<label>Puhelin:</label>
-		<input type="text" name="teacher_phone" value="<?php echo $teacher_phone; ?>" />
-		<?php if ($id != "") { ?>
-			<label>Aseta salasana:</label>
-			<input type="checkbox" name="set_password" value=""/><br>
-		<?php 
-			} else {
-        ?>
-        	<input type="hidden" name="set_password" value="1"/>
-        <?php
-			}	
-		?>
-		<label>Salasana:</label>
-		<input type="password" name="teacher_password" value="" />
-		<label>Salasanan vahvistus:</label>
-		<input type="password" name="teacher_password_confirm" value="" />
-		<input type="submit" value="Talleta"/>
-	</form>	
-<?php		
-   }
-   include("db_disconnect_inc.php");
+	include("sub/teacher/teacher_listing_table.php");
+	include("sub/teacher/modify_or_add_teacher_form.php");
+   	
+   	include("db_disconnect_inc.php");
 ?>
---
+++ html/inc/sub/teacher/teacher_listing_table.php
@@ -0,0 +1,45 @@
+<?php
+	$seek_params_hidden_inputs = hidden_input("seek_first_name", $seek_first_name).
+	                             hidden_input("seek_last_name", $seek_last_name);
+?>
+    <h2>Etsityt opettajat</h2>
+<?php
+   	$sql_seek = "SELECT * FROM ca_staff WHERE Permission = 0 AND Active = 1 ";
+   	$sql_seek = add_further_seek_param($sql_seek, "firstName", $seek_first_name);
+   	$sql_seek = add_further_seek_param($sql_seek, "lastName", $seek_last_name);
+		
+   	if ($result = $conn->query($sql_seek)) {		
+		
+?>
+	<div id="active_staff_table">
+		<div class="row">
+			<div class="col-sm-3"><b>Etunimi</b></div>
+			<div class="col-sm-3"><b>Sukunimi</b></div>
+			<div class="col-sm-3"><b>Sähköposti</b></div>
+			<div class="col-sm-2"><b>Puhelin</b></div>
+			<div class="col-sm-1"><b>Muokkaa</b></div>
+		</div>
+
+<?php   	
+		while($res = $result->fetch_assoc()) {
+?>
+			<div class="row">
+				<div class="col-sm-3"><?php echo $res['FirstName']; ?></div>
+				<div class="col-sm-3"><?php echo $res['LastName']; ?></div>
+				<div class="col-sm-3"><?php echo $res['Email']; ?></div>
+				<div class="col-sm-2"><?php echo $res['PhoneNumber']; ?></div>
+				<div class="col-sm-1">
+					<form method="post" action="list_teachers.php">
+					<input type="hidden" name="id" value="<?php echo $res['ID']; ?>"/>
+<?php echo $seek_params_hidden_inputs; ?>					
+					<input type="submit" value="Muokkaa" />
+					</form>
+				</div>				
+			</div>
+<?php		
+		}
+?>
+	</div>
+<?php
+}
+?>

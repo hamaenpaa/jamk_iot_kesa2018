5a3663081c9dd981f3cb5456cf87ddf10763a01f
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-07-20 12:10:05 +0300

    No students information given better

--- html/inc/sub/student/student_listing_table.php
+++ html/inc/sub/student/student_listing_table.php
@@ -12,45 +12,52 @@
    		$sql_seek = add_further_seek_param($conn, $sql_seek, "Student_ID", $seek_student_id);
 		
    		if ($result = $conn->query($sql_seek)) {
+   			if (mysqli_num_rows($result) > 0) {   
 ?>
-			<div id="student_table">
-				<div class="row">
-					<div class="col-sm-2"><b>Etunimi</b></div>
-					<div class="col-sm-2"><b>Sukunimi</b></div>
-					<div class="col-sm-2"><b>Sähköposti</b></div>
-					<div class="col-sm-2"><b>Puhelin</b></div>
-					<div class="col-sm-1"><b>Oppilas ID</b></div>
-					<div class="col-sm-1"><b>NFC ID</b></div>
-					<div class="col-sm-1"><b>Muokkaa</b></div>
-					<div class="col-sm-1"><b>Poista</b></div>
-				</div>
-<?php   	
-				while($res = $result->fetch_assoc()) {
-?>
+				<div id="student_table">
 					<div class="row">
-						<div class="col-sm-2"><?php echo $res['FirstName']; ?></div>
-						<div class="col-sm-2"><?php echo $res['LastName']; ?></div>
-						<div class="col-sm-2"><?php echo $res['Email']; ?></div>
-						<div class="col-sm-2"><?php echo $res['PhoneNumber']; ?></div>
-						<div class="col-sm-1"><?php echo $res['Student_ID']; ?></div>
-						<div class="col-sm-1"><?php echo $res['NFC_ID']; ?></div>
-						<div class="col-sm-1">
-							<form method="post" action="list_students.php">
-								<input type="hidden" name="id" value="<?php echo $res['ID']; ?>"/>
-<?php echo $seek_params_hidden_inputs; ?>					
-								<input type="submit" value="Muokkaa" />
-							</form>
-						</div>	
-						<div class="col-sm-1">
-							<form method="post" action="inc/sub/student/remove_student.php">
-								<input type="hidden" name="id" value="<?php echo $res['ID']; ?>"/>
-<?php echo $seek_params_hidden_inputs; ?>					
-								<input type="submit" value="Poista" />
-							</form>
-						</div>										
+						<div class="col-sm-2"><b>Etunimi</b></div>
+						<div class="col-sm-2"><b>Sukunimi</b></div>
+						<div class="col-sm-2"><b>Sähköposti</b></div>
+						<div class="col-sm-2"><b>Puhelin</b></div>
+						<div class="col-sm-1"><b>Oppilas ID</b></div>
+						<div class="col-sm-1"><b>NFC ID</b></div>
+						<div class="col-sm-1"><b>Muokkaa</b></div>
+						<div class="col-sm-1"><b>Poista</b></div>
 					</div>
+<?php   	
+					while($res = $result->fetch_assoc()) {
+?>
+						<div class="row">
+							<div class="col-sm-2"><?php echo $res['FirstName']; ?></div>
+							<div class="col-sm-2"><?php echo $res['LastName']; ?></div>
+							<div class="col-sm-2"><?php echo $res['Email']; ?></div>
+							<div class="col-sm-2"><?php echo $res['PhoneNumber']; ?></div>
+							<div class="col-sm-1"><?php echo $res['Student_ID']; ?></div>
+							<div class="col-sm-1"><?php echo $res['NFC_ID']; ?></div>
+							<div class="col-sm-1">
+								<form method="post" action="list_students.php">
+									<input type="hidden" name="id" value="<?php echo $res['ID']; ?>"/>
+<?php echo $seek_params_hidden_inputs; ?>					
+									<input type="submit" value="Muokkaa" />
+								</form>
+							</div>	
+							<div class="col-sm-1">
+								<form method="post" action="inc/sub/student/remove_student.php">
+									<input type="hidden" name="id" value="<?php echo $res['ID']; ?>"/>
+<?php echo $seek_params_hidden_inputs; ?>					
+									<input type="submit" value="Poista" />
+								</form>
+							</div>										
+						</div>
 <?php		
 				}
+			}
+			else {
+?>
+				<b>Haulla ei löytynyt oppilaita</b>
+<?php				
+			} 
 ?>
 			</div>
 <?php

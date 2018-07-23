a19696a5fc936a878f9a5faa0f0136f05e2da4c2
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-07-23 13:52:42 +0300

    required, autofocus and maxlength params added for student htmls, server side length validations

--- html/inc/sub/student/modify_or_add_student_form.php
+++ html/inc/sub/student/modify_or_add_student_form.php
@@ -30,17 +30,17 @@
 	<form method="post" action="inc/sub/student/save_student.php">
 		<input type="hidden" name="id" value="<?php echo $id; ?>" />
 		<label>Tunnus:</label>
-		<input type="text" name="student_id" value="<?php echo $student_id; ?>" />
+		<input type="text" name="student_id" value="<?php echo $student_id; ?>" maxlength="6" required autofocus />
 		<label>Etunimi:</label>
-		<input type="text" name="student_firstname" value="<?php echo $student_firstname; ?>" />
+		<input type="text" name="student_firstname" value="<?php echo $student_firstname; ?>" maxlength="25" required />
 		<label>Sukunimi:</label>
-		<input type="text" name="student_lastname" value="<?php echo $student_lastname; ?>" />
+		<input type="text" name="student_lastname" value="<?php echo $student_lastname; ?>" maxlength="25" required />
 		<label>Sähköposti:</label>
-		<input type="text" name="student_email" value="<?php echo $student_email; ?>" />
+		<input type="email" name="student_email" value="<?php echo $student_email; ?>" maxlength="255" />
 		<label>Puhelin:</label>
-		<input type="text" name="student_phone" value="<?php echo $student_phone; ?>" />
+		<input type="tel" name="student_phone" value="<?php echo $student_phone; ?>" maxlength="13" />
 		<label>NFC ID:</label>
-		<input type="text" name="student_nfcid" value="<?php echo $student_nfcid; ?>" />
+		<input type="text" name="student_nfcid" value="<?php echo $student_nfcid; ?>" maxlength="50" />
 <?php echo $seek_params_hidden_inputs; ?>
 		<input type="submit" value="Talleta"/>
 	</form>
--- html/inc/sub/student/save_student.php
+++ html/inc/sub/student/save_student.php
@@ -8,7 +8,7 @@
 	$seek_params_get = possible_get_param("seek_student_id",$seek_student_id);
 	$seek_params_get .= possible_get_param("seek_first_name",$seek_first_name, $seek_params_get == "");
 	$seek_params_get .= possible_get_param("seek_last_name",$seek_last_name, $seek_params_get == "");
-
+	
 	$id = "";
 	if (isset($_POST['id'])) {
 		$id = strip_tags($_POST['id']);
@@ -19,7 +19,15 @@
 	$student_email = strip_tags($_POST['student_email']);
 	$student_phone = strip_tags($_POST['student_phone']);
 	$student_nfcid = strip_tags($_POST['student_nfcid']);
-	
+
+	if (strlen($student_id) > 6 || strlen($student_firstname) > 25 ||
+		strlen($student_lastname) > 25 || strlen($student_email) > 255 ||
+		strlen($student_phone) > 13 || strlen($student_nfcid) > 50 || 
+		strlen($seek_first_name) > 25 || strlen($seek_last_name) > 25 ||
+		strlen($seek_student_id) > 6) {
+		header("Location: ../../../list_students.php".$seek_params_get);
+		exit;
+	}
 	
 	if ($id != "") {
 		$q = $conn->prepare("UPDATE ca_student SET student_ID = ?, FirstName = ?, LastName = ?, Email=?, PhoneNumber=?, NFC_ID=? WHERE ID = ?");

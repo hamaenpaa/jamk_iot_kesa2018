74ef7a49de1c631cedea9315dcb33a26bfdef1f8
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-07-23 17:23:15 +0300

    Guest limited max count of rooms allowed for query

--- html/inc/sub/room/room_listing_table.php
+++ html/inc/sub/room/room_listing_table.php
@@ -1,12 +1,29 @@
 <?php
+	define("MAX_ROOMS_AT_SEARCH", 50);
+
+    $sql_count_seek = "SELECT COUNT(*) AS c FROM ca_room WHERE removed=0 ";
+   	$sql_count_seek = add_further_seek_param($conn, $sql_count_seek, "room_name", $seek_room_name);
+
    	$sql_seek = "SELECT * FROM ca_room WHERE removed=0 ";
    	$sql_seek = add_further_seek_param($conn, $sql_seek, "room_name", $seek_room_name);
+	$sql_seek .= " LIMIT " . MAX_ROOMS_AT_SEARCH;
+	
+	$result_count = $conn->query($sql_count_seek);
+	$res_count = $result_count->fetch_assoc();
+	$count = $res_count['c'];
+	$rooms_text = "luokka";	
+	if ($count > 1) { $rooms_text .= "a"; }
+	
    	if ($result = $conn->query($sql_seek)) {
 ?>
 		<h2>Etsityt luokat</h2>
 <?php   		
-   		if (mysqli_num_rows($result) > 0) { 
+		$count_rows = mysqli_num_rows($result);
+		if ($count_rows > 0) {  
 ?>
+			<div id="count_of_results">Haussa löytyi 
+				<?php echo $count." ".$rooms_text ?>.
+			</div>
 			<div id="room_table">
 				<div class="row">
 					<div class="col-sm-10"><b>Huoneen tunnus</b></div>
@@ -34,6 +51,18 @@
 						</div>
 					</div>
 <?php		
+				}
+
+				if ($count_rows < $count) {
+?>
+					<div class="row">
+						<div id="room_query_count_exceeded" class="col-sm-12">
+						<b>
+						Haussa tuli yli <?php echo MAX_ROOMS_AT_SEARCH; ?> kurssia. 
+						Vain ensimmäiset <?php echo MAX_ROOMS_AT_SEARCH; ?> näytetään. Tarkenna hakua.</b>
+						</div>
+					</div>
+<?php				
 				}
 		}
 		else {

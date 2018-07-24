766d050239e31aced8bddd0b27739c22868e417e
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-07-24 14:46:56 +0300

    Fix: paging to work with guest remove

--- html/inc/sub/guest/remove_guest.php
+++ html/inc/sub/guest/remove_guest.php
@@ -6,8 +6,16 @@
  	
     $seek_first_name = get_post_or_get($conn, "seek_first_name");
 	$seek_last_name = get_post_or_get($conn, "seek_last_name");
+	
+	if (isset($_POST['page'])) {
+		$page = strip_tags($_POST['page']);
+	} else {
+		$page = 1;
+	}		
+	
 	$seek_params_get .= possible_get_param("seek_first_name",$seek_first_name, $seek_params_get == "");
 	$seek_params_get .= possible_get_param("seek_last_name",$seek_last_name, $seek_params_get == "");
+	$seek_params_get .= possible_get_param("page", $page, $seek_params_get == "");
 
 	$q = $conn->prepare("UPDATE ca_guest SET removed = 1 WHERE ID = ?");
 	if ($q) {

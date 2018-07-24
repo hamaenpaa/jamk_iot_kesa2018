6f5be96a5ef9fa86ee46b4ef0392e90e429ce726
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-07-24 14:43:47 +0300

    Fix: paging to work with room remove

--- html/inc/sub/room/remove_room.php
+++ html/inc/sub/room/remove_room.php
@@ -5,7 +5,15 @@
 	$id = get_post_or_get($conn, "id");
  	
     $seek_room_name = get_post_or_get($conn, "seek_room_name");
+	
+	if (isset($_POST['page'])) {
+		$page = strip_tags($_POST['page']);
+	} else {
+		$page = 1;
+	}		
+	
 	$seek_params_get = possible_get_param("seek_room_name",$seek_room_name);
+	$seek_params_get .= possible_get_param("page", $page, $seek_params_get == "");
 
 	$q = $conn->prepare("UPDATE ca_room SET removed = 1 WHERE ID = ?");
 	if ($q) {

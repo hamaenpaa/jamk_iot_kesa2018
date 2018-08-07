6e31a7b4eab78593d0a5a23743ad07c83182879e
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-08-07 15:01:32 +0300

    First removal based on MVP; not tested

--- html/inc/sub/room/seek_rooms_form.php
+++
@@ -1,9 +0,0 @@
-<div id="seek_rooms">
-    <h2>Etsi luokkia</h2>
-	<form action="list_rooms.php" method="post">
-		<label>Tunnus:<label>
-		<input type="text" name="seek_room_name" value="<?php echo $seek_room_name; ?>" 
-			   maxlength="40" />
-		<input class="button" type="submit" value="Hae" /> 
-	</form>
-</div>
--- html/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-//Poor man's logout
-if (isset($_SESSION)) {
-session_destroy(); //DESTROY THEM ALL
-}
-echo "<meta http-equiv=\"refresh\" content=\"1;url=index.php\" />";
-echo "<p>You have logged out!</p>";
-?>

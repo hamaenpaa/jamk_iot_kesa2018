9fc51a8320b3fdb0aa091e0afc69ab50270ee587
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-08-08 11:30:52 +0300

    Further cutting off extra code towards MVP

--- test/testdata/test_lessons.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO ca_lesson (room_id, course_id, begin_time, end_time) 
-VALUES (11,1, '2018-07-27 09:00:00', '2018-07-27 10:00:00');
--- test/utils/passwordgenerator.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo " admin user<br/>";
-    echo hash("SHA256", "afsrt");
-	echo "<br/><br/>  niko<br/>";
-	echo hash("SHA256", "dtxrt");
-	echo "<br/><br/>  mikko<br/>";
-	echo hash("SHA256", "rnart");
-?>

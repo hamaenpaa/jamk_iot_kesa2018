90d8a3170c4a4f2f33c03c37122cf75869825ff6
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-08-14 15:38:02 +0300

    Original project text references removed from footer

--- html/inc/footer.php
+++ html/inc/footer.php
@@ -1,12 +1,8 @@
 			<footer>
 				<div class="footer-wrap">
 					<div class="footer-links">
-						<p>Linkkejä</p>
 					</div>
 					<div class="footer-contacts">
-						<p>&#169; IoT projekti kesä 2018</p>
-						<p>JAMKin logo</p>
-						<p>Yhteystietoja yms.</p>
 					</div>
 				</div>
 			</footer>

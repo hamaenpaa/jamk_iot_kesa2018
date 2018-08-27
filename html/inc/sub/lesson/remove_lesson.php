788d8b869dd7ea4199e6aa4d19fe06cd36e16851
Harri Mäenpää <harri.maenpaa@gmail.com>
2018-08-27 18:45:43 +0300

    session checks for lessons

--- html/inc/sub/lesson/remove_lesson.php
+++ html/inc/sub/lesson/remove_lesson.php
@@ -1,4 +1,11 @@
 <?php
+	if (!isset($_SESSION)) {
+		session_start();	
+	} 
+	if (!isset($_SESSION['user_id'])) {
+		return;	
+	} 
+
     include("../../db_connect_inc.php");
 	include("../../utils/request_param_utils.php");
 	
